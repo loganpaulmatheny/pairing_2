35de66081faf4c71a84d48c19a3c1ec6928bf1f4
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-22 16:22:50 -0400

    Add places

--- friends.js
+++ friends.js
@@ -3,3 +3,9 @@ var arrays = ["James", "Ronald", "Mary", "Andy", "Logan"];
 console.log(arrays[2]);
 console.log(arrays[arrays.length - 1]);
 console.log(arrays[0]);
+
+var places = ["Japan", "Portugal", "Switzerland", "Hong Kong", "Vietnam"];
+
+places.push("Ireland");
+
+console.log(places);
